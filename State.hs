b2d3e1bf43914a512dc3550fc1174b57983f909a
Ivo Klein Hofmeijer <ivo.klein.h@gmail.com>
2014-10-08 12:09:47 +0200

    small update at meeting

--- State.hs
+++ State.hs
@@ -1,18 +1,30 @@
 data Marker = Zero
+    deriving Show
 
 data SenseDir =
     Here
     | Ahead
     | LeftAhead
     | RightAhead
+    deriving Show
 
 data MoveDir = Left | Right
 
-data State = State [String]
+data State = State { buildState :: Int -> [String] }
 
 -- Go to state st1 if cond holds in sensedir; and to state st2 otherwise.
 sense :: SenseDir -> State -> State -> Marker -> State
-sense = undefined
+sense sensedir st1 st2 marker = State (\index -> let
+                                        st1Index = index + 1 
+                                        st1Code  = buildState st1 st1Index
+                                        st2Index = index + length st1Code + 1
+                                        st2Code  = buildState st2 st2Index
+                                        command  = "Sense " 
+                                                   ++ (show sensedir) 
+                                                   ++ " " ++ (show st1Index) 
+                                                   ++ " " ++ (show st2Index) 
+                                                   ++ (show marker)
+                                    in (command:(st1Code ++ st2Code)))
 
 -- Set mark i in current cell and go to st.
 mark :: Marker -> State -> State
@@ -41,3 +53,4 @@ move = undefined
 -- Choose a random number x from 0 to p-1 ; go to st1 if x=0 and st2 otherwise.
 flip :: Int -> State -> State -> State
 flip = undefined
+
