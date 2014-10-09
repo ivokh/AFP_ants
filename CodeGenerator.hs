3cb8572ed8cf062c41a1a9961b7cf6c769184853
Koen Wermer <koenwermer@gmail.com>
2014-10-09 14:16:47 +0200

    abstraheren met de reader monad, de environment wordt nu achter de schermen doorgegeven ipv handmatig

--- CodeGenerator.hs
+++ CodeGenerator.hs
@@ -1,9 +1,10 @@
 {-#LANGUAGE TypeSynonymInstances, FlexibleInstances#-}
 module CodeGenerator where
 
-import Control.Monad.State
 import Data.Maybe
 import Control.Applicative
+import Control.Monad.State
+import Control.Monad.Reader
 
 type Marker = Int
 
@@ -41,7 +42,10 @@ instance Show MoveDir where
 
 type Env = [(AntState, Int)]
 
-type Code = State (Int, Env) [Instruction]
+type Code' a = ReaderT Env (State (Int, Env)) a
+
+-- | The Code monad produces instructions, using the current environment and antstate index, and builds the environment while doing so
+type Code = Code' [Instruction]
 
 -- | We can use combine to combine functions, i.env. concatenate code
 class Combine a where
@@ -52,23 +56,27 @@ instance Combine Code where
     
 instance Combine a => Combine (Code -> a) where
     combine c1 c2 = combine $ (++) <$> c1 <*> c2
+    
+runCode :: Code -> [Instruction]
+runCode c = let (instr, (_, env)) = runState (runReaderT c env) (0, []) in instr
 
 -- | Represents the next Instruction
 next :: AntState
 next = Relative 1
     
 -- | Looks up an AntState in the environment
-lookup' :: Env -> AntState -> State (Int, [(AntState, Int)]) Int
-lookup' env st = 
+lookup' :: AntState -> Code' Int
+lookup' st = 
     do
         (index, _) <- get
+        env <- ask
         case st of
             Relative n -> return (index + n)
-            _           -> return (fromJust $ lookup st env)
+            _          -> return (fromJust $ lookup st env)
 
 -- | Lifts instructions to code and also updates the index
 return' :: Instruction -> Code
-return' instr =
+return' instr = ReaderT $ \r ->
     do
         (index, table) <- get
         put (index + 1, table)
@@ -76,7 +84,7 @@ return' instr =
 
 -- | Labels a function
 annotate :: String -> Code -> Code
-annotate s code =
+annotate s code = 
     do
         (index, table) <- get
         put (index, (Jump s, index):table)
@@ -84,61 +92,61 @@ annotate s code =
         return instr
 
 -- | Go to Code st1 if cond holds in sensedir; and to Code st2 otherwise.
-sense :: Env -> SenseDir -> AntState -> AntState -> Condition -> Code
-sense env sensedir st1 st2 cond = 
+sense :: SenseDir -> AntState -> AntState -> Condition -> Code
+sense sensedir st1 st2 cond = 
     do
-        st1' <- lookup' env st1
-        st2' <- lookup' env st2
+        st1' <- lookup' st1
+        st2' <- lookup' st2
         return' $ Sense sensedir st1' st2' cond
 
 -- | Set mark i in current cell and go to st.
-mark :: Env -> Marker -> AntState -> Code
-mark env n st = 
+mark :: Marker -> AntState -> Code
+mark n st = 
     do
-        st' <- lookup' env st
+        st' <- lookup' st
         return' $ Mark n st'
 
 -- | Clear mark i in current cell and go to st.
-unMark :: Env -> Marker -> AntState -> Code
-unMark env n st = 
+unMark :: Marker -> AntState -> Code
+unMark n st = 
     do
-        st' <- lookup' env st
+        st' <- lookup' st
         return' $ Unmark n st'
 
 -- | Pick up food from current cell and go to st1 ; go to st2 if there is no food in the current cell.
-pickUp :: Env -> AntState -> AntState -> Code
-pickUp env st1 st2 = 
+pickUp :: AntState -> AntState -> Code
+pickUp st1 st2 = 
     do
-        st1' <- lookup' env st1
-        st2' <- lookup' env st2
+        st1' <- lookup' st1
+        st2' <- lookup' st2
         return' $ PickUp st1' st2'
 
 -- | Drop food in current cell and go to st.
-dropFood :: Env -> AntState -> Code
-dropFood env st = 
+dropFood :: AntState -> Code
+dropFood st = 
     do
-        st' <- lookup' env st
+        st' <- lookup' st
         return' $ Drop st'
 
 -- | Turn left or right and go to st.
-turn :: Env -> MoveDir -> AntState -> Code
-turn env dir st =  
+turn :: MoveDir -> AntState -> Code
+turn dir st =  
     do
-        st' <- lookup' env st
+        st' <- lookup' st
         return' $ Turn dir st'
 
 -- | Move forward and go to st1 ; go to st2 if the cell ahead is blocked.
-move :: Env -> AntState -> AntState -> Code
-move env st1 st2 = 
+move :: AntState -> AntState -> Code
+move st1 st2 = 
     do
-        st1' <- lookup' env st1
-        st2' <- lookup' env st2
+        st1' <- lookup' st1
+        st2' <- lookup' st2
         return' $ Move st1' st2'
 
 -- | Choose a random number x from 0 to p-1 ; go to st1 if x=0 and st2 otherwise.
-toss :: Env -> Int -> AntState -> AntState -> Code
-toss env n st1 st2 = 
+toss :: Int -> AntState -> AntState -> Code
+toss n st1 st2 = 
     do
-        st1' <- lookup' env st1
-        st2' <- lookup' env st2
+        st1' <- lookup' st1
+        st2' <- lookup' st2
         return' $ Flip n st1' st2'
--- Funtions.hs
+++ Funtions.hs
@@ -16,24 +16,21 @@ Some (possibly useless) funtions for testing:
 
 main = 
     do
-        let code = unlines $ map show instructions
+        let code = unlines $ map show (runCode test)
         writeFile "testCode.txt" code
 
-instructions :: [Instruction]
-instructions = let (instr, (_, env)) = runState (test env) (0, []) in instr
-
-test :: Env -> Code
-test env = annotate "test" $ combine
-                             (toss env 2 (Jump "init1") (Jump "init2")) --Split the ants in 2 groups of approximately the same size
-                             (annotate "init1" (init1 env))
-                             (annotate "init2" (init2 env))
+test :: Code
+test = annotate "test" $ combine
+        (toss 2 (Jump "init1") (Jump "init2")) --Split the ants in 2 groups of approximately the same size
+        (annotate "init1" init1)
+        (annotate "init2" init2)
     
 --initialize group 1
-init1 :: Env -> Code
-init1 env = combine
-    (fmap concat . replicateM 10 $ dropFood env next) --just stall 10 turns trying to drop things..
-    (turn env L (Jump "test")) -- ..turn left and them toss a coin again :D
+init1 :: Code
+init1 = combine
+    (fmap concat . replicateM 10 $ dropFood next) --just stall 10 turns trying to drop things..
+    (turn L (Jump "test")) -- ..turn left and them toss a coin again :D
     
 --initialize group 2
-init2 :: Env -> Code
-init2 env = fmap concat . replicateM 100 $ move env next next --MOVE MOVE MOVE!!
+init2 :: Code
+init2 = fmap concat . replicateM 100 $ move next next --MOVE MOVE MOVE!!
