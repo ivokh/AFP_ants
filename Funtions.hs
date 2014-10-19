8a68037a9644e6a2e3e4b029126f049d5cb7c633
Koen Wermer <koenwermer@gmail.com>
2014-10-19 23:29:34 +0200

    verder gewerkt aan voedsel zoeken

--- Funtions.hs
+++ Funtions.hs
@@ -8,39 +8,59 @@ import Control.Monad.State
 
 type Dir = Int --0 (east) to 5 (north-east), clockwise
 
+foodPath :: Condition
+foodPath = Marker 0
+
 main = 
     do
         let code = unlines . map show . runCode $ combine
-                searchFoodDef
+                --Start with assigning roles
+                (assignRandom [call "searchFood" [mkParam 0, mkParam dir] | dir <- [0, 2, 4]])
+                --All other code comes here (note that all states have the same properties, except for state 1 where execution starts)
                 takeAndGoHomeDef
                 followHomeDef
-                (searchFood 0 2) --TODO: different ants should go in different directions
+                startFollowFoodDef
+                followToFoodDef
+                searchFoodDef
         writeFile "pathFollowing2.txt" code
+        
+-- | Assigns random roles to ants, the list must be length 2 or larger, every antstate in the list has the same chance of being selected
+assignRandom :: [AntState] -> Code
+assignRandom xs = assignRandom' (length xs) xs where
+    assignRandom' 2 (x:y:[]) = toss 2 x y
+    assignRandom' n (x:xs)   = combine (toss n x next) (assignRandom' (n - 1) xs)
 
 -- | Defines searchFood for all legitimate parameters
 searchFoodDef :: Code
 searchFoodDef = combineList [searchFood facing dest | facing <- [0, 2, 4], dest <- [0, 2, 4]]
 
 --TODO: jump to a different state if moving fails
-------- sense even markers, and follow them (and stop leaving odd markers)
 ------- change direction a bit every now and them
 -- | Searches for food given the dir it's facing (this should be even!) and the (even) dir it should go, and leaves a trail that can be followed home
 searchFood :: Dir -> Dir -> Code
 searchFood facing dest = define "searchFood" [mkParam facing, mkParam dest] $ combine 
+    --Turn to the assigned direction
     (turnN' (shortestTurn (dest - facing)))
+    --Sense food
     (sense Here (call "takeAndGoHome" [mkParam facing]) next Food)
+    --Sense a path leading to food
+    (sense Here (call "startFollowFood" [mkParam facing]) next foodPath)
+    --If food or path wasn't found, leave a mark that leads to home and move
     (mark ((dest + 3) `mod` 6) next)
-    (move (call "searchFood" [mkParam facing, mkParam dest]) this) 
+    (move (call "searchFood" [mkParam dest, mkParam dest]) next) 
+    --If movement fails, pick a random new even direction
+    (toss 2 (call "searchFood" [mkParam dest, mkParam ((dest - 2) `mod` 6)]) (call "searchFood" [mkParam dest, mkParam ((dest + 2) `mod` 6)]))
         where
             turnN' n | n > 0     = combineList $ replicate n    (turn R next)
                      | n < 0     = combineList $ replicate (-n) (turn L next)
                      | otherwise = return []
             
-
+-- | Gives the shortest turn for an int, where possitive means clockwise and negative means counter-clockwise
 shortestTurn :: Int -> Int
-shortestTurn n | n > 3     = 6 - n
-               | n < -3    = 6 + n
-               | otherwise = n
+shortestTurn n | m > 3     = 6 - m
+               | m < -3    = 6 + m
+               | otherwise = m
+    where m = n `mod` 6
 
 takeAndGoHomeDef :: Code
 takeAndGoHomeDef = combineList [takeAndGoHome facing | facing <- [0, 2, 4]]
@@ -49,8 +69,8 @@ takeAndGoHomeDef = combineList [takeAndGoHome facing | facing <- [0, 2, 4]]
 takeAndGoHome :: Dir -> Code
 takeAndGoHome facing = define "takeAndGoHome" [mkParam facing] $ combine
     (pickUp next next) --TODO search food nearby if this food particle is gone
-    (combineList (replicate 3 (turn L next)))
-    (move (call "followHome" [mkParam ((facing + 3) `mod` 6)]) this)
+    (combineList (replicate 2 (turn L next)))
+    (turn L (call "followHome" [mkParam ((facing + 3) `mod` 6)]))
     
 followHomeDef :: Code
 followHomeDef = combineList ([followHome facing | facing <- [1, 3, 5]] ++ [turnNDef (call "followHome" [mkParam facing]) | facing <- [1, 3, 5]])
@@ -58,12 +78,34 @@ followHomeDef = combineList ([followHome facing | facing <- [1, 3, 5]] ++ [turnN
 -- | Follows a path of odd markers, leaving a trail of odd markers, given the direction it's facing (should be odd)
 followHome :: Dir -> Code
 followHome facing = define "followHome" [mkParam facing] $ combine
+    (move next this) --Todo: handle collisions with other ants
     (follow 1)
     (follow 3)
     (follow 5)
-        --TODO: what if there is no marker for some reason?
         where follow n = sense Here (call "turnN" [mkParam $ shortestTurn (n - facing), mkParam $ call "followHome" [mkParam n]]) next (Marker n)
     
+startFollowFoodDef :: Code
+startFollowFoodDef = combineList [startFollowFood facing | facing <- [0..5]]
+    
+-- | Starts following a path to food
+startFollowFood :: Dir -> Code
+startFollowFood facing = define "startFollowFood" [mkParam facing] $
+    combineList [follow n | n <- [1, 3, 5]]
+        where follow n = sense Here (call "turnN" [mkParam $ shortestTurn (n + 3 - facing), mkParam $ call "followToFood" [mkParam ((n + 3) `mod` 6)]]) next (Marker n)
+    
+-- | Defines followToFood
+followToFoodDef :: Code
+followToFoodDef = combineList ([followToFood facing | facing <- [0, 2, 4]] ++ [turnNDef (call "followToFood" [mkParam facing]) | facing <- [0, 2, 4]])
+    
+-- | Follows a trail that leads to food, note that paths lead to home, so we need to follow it in the opposite direction
+--Idea: if food is gone, leave a marker for other ants and start clearing the trail of marker 0, other ants go home and search
+followToFood :: Dir -> Code
+followToFood facing = define "followToFood" [mkParam facing] $ combine
+    (move next this) --Todo: handle collisions with other ants
+    (follow 1)
+    (follow 3)
+    (follow 5)
+        where follow n = sense Here (call "turnN" [mkParam $ shortestTurn (n + 3 - facing), mkParam $ call "followToFood" [mkParam ((n + 3) `mod` 6)]]) next (Marker n)
 
 -- | Defines turnN for all legitimate parameters, given the next state
 turnNDef :: AntState -> Code
